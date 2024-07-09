ca534a1114b1b7ff82611f5e87ebf2818919ec3b
Frank Manno <frankmanno@gmail.com>
2024-07-09 22:02:02 +0000

    WIP: initial reports controller

---
+++ app/controllers/reports.php
@@ -0,0 +1,18 @@
+<?php
+
+class Reports extends Controller {
+  public function index() {
+    echo 'reports';
+    die;
+  }
+
+  public function reminders() {
+    echo 'reports/reminders';
+    die;
+  }
+
+  public function logins() {
+    echo 'reports/logins';
+    die;
+  }
+}
